5b9cb1338a1d200e2c8b6cb85f211064e0bf8de8
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-06-03 14:42:39 -0300

    feat: project component

---
+++ frontend/src/components/aboutUs/Project.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import Image from "next/image";
+
+interface ProjectProps {
+  title: string;
+  description: string;
+  image: string;
+}
+
+const Project: FC<ProjectProps> = ({ title, description, image }) => {
+  return (
+    <div className="flex p-2.5 flex-col-reverse lg:flex-row items-center bg-white rounded-lg text-[var(--text-color)]">
+      <div className="flex flex-col px-5 py-10 w-full">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-base">{description}</p>
+      </div>
+
+      <div className="w-full max-h-[200px] aspect-[1/1] relative lg:w-[190px] lg:h-[190px]">
+        <Image
+          src={image}
+          alt="project-image"
+          fill
+          className="object-cover object-center rounded-[6px]"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Project;
