dcd066c46cc4b6b9d92a3cac09fd51c22ef70cb1
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-06-03 14:12:14 -0300

    feat: event date component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,7 @@
-import ContactInfo from "@/components/aboutUs/ContactInfo";
-import { FaWhatsapp } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-[var(--background-color)]">
     </div>
   );
 }
