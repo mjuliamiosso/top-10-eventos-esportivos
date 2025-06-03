6aa8f617ec057486db823399c1ff6194c4d82649
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-06-03 12:09:17 -0300

    feat: contact info component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,9 @@
-import Button from "@/components/common/Button";
-import { FaPlus } from "react-icons/fa6";
+import ContactInfo from "@/components/aboutUs/ContactInfo";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="">
-      <Button>
-        <FaPlus />
-        Ver mais
-      </Button>
     </div>
   );
 }
