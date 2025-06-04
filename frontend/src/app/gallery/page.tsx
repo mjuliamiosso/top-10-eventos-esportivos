4fe6e087eb020812b1297e75b9cad5289fc67f3a
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-06-04 15:00:25 -0300

    feat: news-page

---
+++ frontend/src/app/gallery/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className="bg-[var(--background-color)]">
+      <div className="container sectionSpacing">
+        
+      </div>
+    </section>
+  )
+}
+
+export default page
