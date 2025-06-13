ed68af78cc3c688f0ad74e00ece72f51879d0a30
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-06-13 18:36:32 -0300

    style (button): hover animation

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 const Button: FC<ButtonProps> = ({ children, onClick }) => {
   return (
     <button
-      className="flex items-center gap-2 bg-[var(--secondary-color)] hover:bg-[var(--secondary-hover)] transition-all rounded-lg cursor-pointer font-bold text-[var(--text-white)] px-3 py-2 text-sm lg:px-4 lg:py-3 lg:text-base"
+      className="flex items-center gap-2 bg-[var(--secondary-color)] hover:bg-[var(--secondary-hover)] transition-all duration-300 ease-in-out rounded-lg cursor-pointer font-bold text-[var(--text-white)] px-3 py-2 text-sm lg:px-4 lg:py-3 lg:text-base"
       onClick={onClick}
     >
       {children}
